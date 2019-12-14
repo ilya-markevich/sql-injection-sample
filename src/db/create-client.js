91e55d9c3f52fb33286ccc8ac9ed3b50fca768d4
Ilya Markevich <ilya.markevich1991@gmail.com>
2019-12-14 15:22:10 +0300

    connection with reconnect.

--- src/db/create-client.js
+++ src/db/create-client.js
@@ -1,9 +1,36 @@
 const { Client } = require('pg');
+const logger = require('../helpers/logger');
+
+const CONNECTION_ATTEMPTS_COUNT = 4;
 
 module.exports = (dbOpts) => {
-    const client = new Client(dbOpts);
+    let client;
 
-    const connect = () => client.connect();
+    const delayedConnect = async (delay) => {
+        await new Promise((resolve) => setTimeout(resolve, delay * 1000));
+        client = new Client(dbOpts);
+        await client.connect();
+    };
+
+    const connectWithReconnection = async (attemptNumber) => {
+        if (attemptNumber > CONNECTION_ATTEMPTS_COUNT) {
+            return Promise.reject(new Error("Can't connect to database"));
+        }
+
+        try {
+            const delay = attemptNumber * attemptNumber;
+            logger.info(`Connection to database: attempt ${attemptNumber} with delay ${delay} second(s)`);
+            await delayedConnect(delay);
+            logger.info('Successfully connected');
+            return Promise.resolve();
+        } catch (err) {
+            logger.error(err);
+            await client.end();
+            return connectWithReconnection(attemptNumber + 1);
+        }
+    };
+
+    const connect = () => connectWithReconnection(1);
 
     const disconnect = () => client.end();
 
